f13650667a61227dc212d1ac7b124867ce39230f
Pete Inge <pete.inge@gmail.com>
2019-07-03 13:59:07 -0400

    Add caching #2

--- src/Controller/ClientView.php
+++ src/Controller/ClientView.php
@@ -7,6 +7,8 @@ use Drupal\Core\Controller\ControllerBase;
 use Github\Client as GithubClient;
 use Drupal\bluecadet_issue_tracker\ZenHubAPI;
 
+define("BCIT_CACHETIME", 3600);
+
 /**
  *
  */
@@ -16,6 +18,9 @@ class ClientView extends ControllerBase {
   private $github_org = "";
   private $github_proj = "";
   private $github_label = "";
+
+  private $client;
+  private $zen;
 
   /**
    *
@@ -28,11 +33,16 @@ class ClientView extends ControllerBase {
       $this->github_org = $settings['bcit']['github']['github_org'];
       $this->github_proj = $settings['bcit']['github']['github_proj'];
       $this->github_label = $settings['bcit']['github']['github_label'];
+
+      $this->client = new GithubClient();
+      $this->client->authenticate($this->github_token, NULL, GithubClient::AUTH_HTTP_TOKEN);
+
+      $this->zen = new ZenHubAPI();
     }
   }
 
   /**
-   *
+   * Build the client view page.
    */
   public function build() {
 
@@ -49,18 +59,16 @@ class ClientView extends ControllerBase {
       ],
     ];
 
-    $client = new GithubClient();
-    $client->authenticate($this->github_token, NULL, GithubClient::AUTH_HTTP_TOKEN);
+    $issues = $this->getGithubIssues();
 
-    $issues = $client->api('issue')->all($this->github_org, $this->github_proj, ['labels' => $this->github_label]);
     $processed_issues = [];
     foreach ($issues as $issue) {
       $processed_issues[$issue['number']] = $issue;
     }
 
-    $zen = new ZenHubAPI();
-    $boards = $zen->getAllBoards();
-    // ksm($boards);
+    // Get The ZenHub Boards.
+    $boards = $this->getZenHubBoards();
+
     if (!empty($boards)) {
       foreach ($boards->pipelines as &$board) {
         $boards->to_show = [];
@@ -83,7 +91,7 @@ class ClientView extends ControllerBase {
       }
     }
 
-    $closed_issues = $client->api('issue')->all($this->github_org, $this->github_proj, ['labels' => $this->github_label, 'state' => 'closed']);
+    $closed_issues = $this->getGithubClosedIssues();
 
     $build['issues']["closed"] = [
       '#theme' => 'item_list',
@@ -97,6 +105,57 @@ class ClientView extends ControllerBase {
     }
 
     return $build;
+  }
+
+  protected function getGithubIssues() {
+
+    $cid = "issues";
+    if ($cache = \Drupal::cache('bcit_issue_tracker')->get($cid)) {
+      // drupal_set_message("Issues Cache hit");
+      $issues = $cache->data;
+    }
+    else {
+      // drupal_set_message("Issues Cache miss");
+      $issues = $this->client->api('issue')->all($this->github_org, $this->github_proj, ['labels' => $this->github_label]);
+
+      \Drupal::cache('bcit_issue_tracker')->set($cid, $issues, (time() + BCIT_CACHETIME), ["bcit:github", "bcit:issues"]);
+    }
+
+    return $issues;
+  }
+
+  protected function getGithubClosedIssues() {
+
+    $cid = "issues:closed";
+    if ($cache = \Drupal::cache('bcit_issue_tracker')->get($cid)) {
+      // drupal_set_message("CIssues Cache hit");
+      $issues = $cache->data;
+    }
+    else {
+      // drupal_set_message("CIssues Cache miss");
+      $issues = $this->client->api('issue')->all($this->github_org, $this->github_proj, ['labels' => $this->github_label, 'state' => 'closed']);
+
+      \Drupal::cache('bcit_issue_tracker')->set($cid, $issues, (time() + BCIT_CACHETIME), ["bcit:github", "bcit:issues", "bcit:issues:closed"]);
+    }
+
+    return $issues;
+  }
+
+  protected function getZenHubBoards() {
+    $boards = [];
+
+    $cid = "z_boards";
+    if ($cache = \Drupal::cache('bcit_issue_tracker')->get($cid)) {
+      // drupal_set_message("Boards Cache hit");
+      $boards = $cache->data;
+    }
+    else {
+      // drupal_set_message("Boards Cache miss");
+      $boards = $this->zen->getAllBoards();
+
+      \Drupal::cache('bcit_issue_tracker')->set($cid, $boards, (time() + BCIT_CACHETIME), ["bcit:zenhub", "bcit:zboards"]);
+    }
+    return $boards;
   }
 
 }
